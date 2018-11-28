2ca25efa222baf44cd44f8bdc02a0fce4ae9af31
KennedyWolfie <kennyjunior022006@yahoo.com>
2018-11-28 03:39:10 +0100

    Updated Data_Collector
    Updated Data_Collector

--- SCRIPTS/Data_Collector.py
+++ SCRIPTS/Data_Collector.py
@@ -10,6 +10,7 @@ __email__ = "kennedyczar@gmail.com"
 __version__ = '1.0'
 
 import os
+from TICKERS import TICKER
 
 class Data_collector(object):
     def __init__(self, path):
@@ -63,7 +64,7 @@ class Data_collector(object):
         finally:
             print('Process completed...Exiting')
            
-    def STOCK_EXTRACTOR(self):
+    def STOCK_EXTRACTOR(self, url):
         '''
         :Functionality:
             Collects stock data using the yahoo API
@@ -74,145 +75,37 @@ class Data_collector(object):
         import pandas as pd
         from datetime import datetime
         
+        self.url = url
         '''Set the start date'''
         self.START = '2010-01-01'
 #        self.END = '2018-10-01'
         '''Create a list of stocks to download'''
-        self.TICKERS_ = ['AMXL.MX',
-                     'WALMEX.MX',
-                     'FEMSAUBD.MX',
-                     'ALFAA.MX',
-                     'CEMEXCPO.MX',
-                     'BIMBOA.MX',
-                     'KOFL.MX',
-                     'GMEXICOB.MX',
-                     'CUERVO.MX',
-                     'GFNORTEO.MX',
-                     'SORIANAB.MX',
-                     'ALPEKA.MX',
-                     'GCARSOA1.MX',
-                     'LIVEPOLC-1.MX',
-                     'TLEVISACPO.MX',
-                     'SAN.MX',
-                     'MEXCHEM.MX',
-                     'ELEKTRA.MX',
-                     'CHDRAUIB.MX',
-                     'AC.MX',
-                     'PE&OLES.MX',
-                     'GFINBURO.MX',
-                     'GRUMAB.MX',
-                     'LACOMERUBC.MX',
-                     'LALAB.MX',
-                     'AEROMEX.MX',
-                     'BACHOCOB.MX',
-                     'GSANBORB-1.MX',
-                     'CULTIBAB.MX',
-                     'GNP.MX',
-                     'FRAGUAB.MX',
-                     'ICHB.MX',
-                     'KIMBERA.MX',
-                     'SIMECB.MX',
-                     'ALSEA.MX',
-                     'GPH1.MX',
-                     'VITROA.MX',
-                     'GIGANTE.MX',
-                     'KUOB.MX',
-                     'ANGELD10.MX',
-                     'OHLMEX.MX',
-                     'Q.MX',
-                     'GENTERA.MX',
-                     'DIABLOI10.MX',
-                     'GFAMSAA.MX',
-                     'IDEALB-1.MX',
-                     'HERDEZ.MX',
-                     'VOLARA.MX',
-                     'AZTECACPO.MX',
-                     'MFRISCOA-1.MX',
-                     'PAPPEL.MX',
-                     'RASSINICPO.MX',
-                     'LABB.MX',
-                     'MEGACPO.MX',
-                     'IENOVA.MX',
-                     'AXTELCPO.MX',
-                     'GCC.MX',
-                     'GISSAA.MX',
-                     'CMOCTEZ.MX',
-                     'BAFARB.MX',
-                     'GPROFUT.MX',
-                     'LAMOSA.MX',
-                     'CERAMICB.MX',
-                     'PINFRA.MX',
-                     'AGUA.MX',
-                     'CIEB.MX',
-                     'ARA.MX',
-                     'POCHTECB.MX',
-                     'ASURB.MX',
-                     'FINDEP.MX',
-                     'POSADASA.MX',
-                     'MINSAB.MX',
-                     'GAPB.MX',
-                     'INVEXA.MX',
-                     'CYDSASAA.MX',
-                     'MONEXB.MX',
-                     'COLLADO.MX',
-                     'UNIFINA.MX',
-                     'ACTINVRB.MX',
-                     'ACCELSAB.MX',
-                     'AUTLANB.MX',
-                     'PASAB.MX',
-                     'OMAB.MX',
-                     'GBMO.MX',
-                     'PV.MX',
-                     'CREAL.MX',
-                     'TMMA.MX',
-                     'MAXCOMA.MX',
-                     'VASCONI.MX',
-                     'FIBRAMQ12.MX',
-                     'GMD.MX',
-                     'CMRB.MX',
-                     'BOLSAA.MX',
-                     'VALUEGFO.MX',
-                     'MEDICAB.MX',
-                     'TERRA13.MX',
-                     'DANHOS13.MX',
-                     'FIHO12.MX',
-                     'CIDMEGA.MX',
-                     'HCITY.MX',
-                     'FIBRAPL14.MX',
-                     'SPORTS.MX',
-                     'DINEB.MX',
-                     'CONVERA.MX',
-                     'VESTA.MX',
-                     'RCENTROA.MX',
-                     'FINN13.MX',
-                     'HOTEL.MX',
-                     'FSHOP13.MX',
-                     'TEAKCPO.MX',
-                     'SAREB.MX',
-                     'FMTY14.MX',
-                     'FHIPO14.MX',
-                     'HOMEX.MX',
-                     'GMXT.MX',
-                     'URBI.MX']
+        self.TICKERS = []
+        self.SYMBOLS = TICKER(self.url).parse()
         
-        
+        for ii in self.SYMBOLS['IB_Symbol'].values:
+          self.TICKERS.append(ii + '{}'.format('.MX'))
+          
         '''write the stock data to specific format by 
         appending the right extension'''
-        STOCK_TICKER_ = pd.DataFrame(self.TICKERS_)
+        STOCK_TICKER_ = pd.DataFrame(self.TICKERS)
         self.FORMAT = ['.csv', '.xlsx', '.json']
         for extension in self.FORMAT:
             STOCK_TICKER_.to_csv('TICKERS/STOCK_TICKER{}'.format(extension))
         print('======= Begin downloading stock dataset ======')
         try:
-            for self.TICK_SYMBOLS in self.TICKERS_:
+            for self.TICK_SYMBOLS in self.TICKERS:
                 '''just in case your connection breaks, 
                 we'd like to save our progress! by appending
                 downloaded dataset to DATASET FOLDER'''
                 if not os.path.exists('DATASET/{}.csv'.format(self.TICK_SYMBOLS)):
+                  try:
                     df = yahoo.download(self.TICK_SYMBOLS, start = self.START, end = datetime.now())
                     df.reset_index(inplace = True)
                     df.set_index("Date", inplace = True)
                     df.to_csv('DATASET/{}.csv'.format(self.TICK_SYMBOLS))
+                  except ValueError:
+                    pass
                 else:
                     print('File Already existing: {}'.format(self.TICK_SYMBOLS))
         except OSError as e:
@@ -224,7 +117,25 @@ class Data_collector(object):
 if __name__ == '__main__':
     '''Define a path on your drive where this project folder is located'''
     path = 'D:\\GIT PROJECT\\ERIC_PROJECT101\\FREELANCE_KENNETH'
-    Data_collector(path).STOCK_EXTRACTOR()
+    base_url = "https://www.interactivebrokers.com/en/index.php?f=2222&exch=mexi&showcategories=STK&p=&cc=&limit=100"
+    Data_collector(path).STOCK_EXTRACTOR(base_url)
     
     
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
